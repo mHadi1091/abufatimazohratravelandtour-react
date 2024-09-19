bd5b09b799181d9958af2538493683f81b030bef
mhadi1091 <mhadimalik2004@gmail.com>
2024-09-19 22:23:31 +0500

    intail commit

--- src/sections/ContactUs.js
+++ src/sections/ContactUs.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { Grid } from "@mui/material";
 
 const ContactUs = () => {
-  return <div></div>;
+  return (
+    <Grid container>
+      <Grid item></Grid>
+    </Grid>
+  );
 };
 
 export default ContactUs;
