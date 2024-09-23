5bd13747509e3b5782e95c2b884c191be6a912fc
mhadi1091 <mhadimalik2004@gmail.com>
2024-09-23 19:24:11 +0500

    intial commit

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,18 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Modal from "../../Modal";
 import "./body.css";
 import PaymentSection from "../../sections/PaymentSection";
 import AboutUs from "../../sections/AboutUs";
 import TransportSection from "../../components/table/TransportSection";
 import { Stack } from "@mui/material";
-import { CustomInputField, MyForm } from "../bookingform/CustomInputField";
 import Bookingform from "../bookingform/Bookingform";
 import Faqs from "../../sections/Faqs";
 import ContactUs from "../../sections/ContactUs";
 
 const Body = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    // Show the modal after 3 seconds
+    const timer = setTimeout(() => {
+      setShowModal(true);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   const Home = {
     label: "WELCOME TO ABU FATIMA ZOHRA TRAVEL AND TOURS",
-    name: "Specialists in Hajj, umarah tours",
+    name: "Specialists in Hajj, umrah tours",
     detailDes:
       "عَنْ أَبِي هُرَيْرَةَ رَضِيَ اللَّهُ عَنْهُ أَنَّ النَّبِيَّ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ قَالَ لَا تُشَدُّ الرِّحَالُ إِلَّا إِلَى ثَلَاثَةِ مَسَاجِدَ الْمَسْجِدِ الْحَرَامِ وَمَسْجِدِي هَذَا وَالْمَسْجِدِ الْأَقْصَى",
     ArabicLabel: "Sahih Bukhari 1132",
@@ -20,7 +37,7 @@ const Body = () => {
   const AboutUsSec = {
     name: "About Us",
     detailDes:
-      "Ready to take the next step? Get in touch with us to plan your journey, and let us handle the details so you can focus on what matters most. Our dedicated team is here to help, whether it's a pilgrimage to Hajj or umarah.",
+      "Ready to take the next step? Get in touch with us to plan your journey, and let us handle the details so you can focus on what matters most. Our dedicated team is here to help, whether it's a pilgrimage to Hajj or umrah.",
   };
   return (
     <div>
@@ -58,6 +75,7 @@ const Body = () => {
           <ContactUs />
         </div>
       </Stack>
+      <Modal showModal={showModal} handleClose={handleCloseModal} />
     </div>
   );
 };
